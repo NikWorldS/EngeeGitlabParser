399bf8d45075406755c47f8c34847d45f26f2ecf
NikWorldS <kvoronin50@yandex.ru>
2025-12-04 12:25:08 +0500

    Update async_parser.py: remove unused variables

--- async_parser.py
+++ async_parser.py
@@ -6,15 +6,9 @@ from Deque import Deque
 
 
 class Parser:
-    #TODO: remove __examples, __reg_examples, __checked_projects
     def __init__(self):
         self.__projects_WEF: list[str] = []
-        self.__checked_projects: set[str] = set()  # staying here for tests
-        self.__examples: dict[str: int] = dict()  # staying here for tests
-        self.__reg_examples: dict[str: int] = {'ru': 0, "zh": 0, "en": 0}  # staying here for tests
         self.__last_project_id = requests.get("https://git.engee.com/api/v4/projects?per_page=1&order_by=id&sort=desc").json()[0].get("id")
-
-
 
     #TODO: возможно, новая функция для сбора ссылок на файлы .engee
     @staticmethod
@@ -46,21 +40,7 @@ class Parser:
                 data = await response.json()
                 if data.get("visibility") == "public":
                     name_with_namespace = data.get("name_with_namespace")
-                    # if " / examples / " in name_with_namespace.lower():
-                    #     if " / zh / " in name_with_namespace.lower():
-                    #         self.__reg_examples["zh"] += 1
-                    #     elif " / en / " in name_with_namespace.lower():
-                    #         self.__reg_examples["en"] += 1
-                    #     else:
-                    #         self.__reg_examples["ru"] += 1
-                    #     self.__checked_projects.add(name_with_namespace)
-                    #     name = data.get("name")
-                    #     if name in self.__examples:
-                    #         self.__examples[name] += 1
-                    #     else:
-                    #         self.__examples[name] = 1
                     if (" / en / " not in name_with_namespace.lower()) and (" / zh / " not in name_with_namespace.lower()):
-
                         project_link = data.get("web_url")
                         async with session.get(url + "/repository/tree") as tree_response:
                             if tree_response.status == 200:
@@ -71,7 +51,7 @@ class Parser:
 
 
     async def main(self) -> None:
-        """Создаёт клиент для запросов и раздаёт задания, в конце записывать все полученные значения в массив"""
+        """Точка входа. Создаёт клиент для запросов и раздаёт задания, в конце записывать все полученные значения в массив."""
         async with aiohttp.ClientSession() as session:
             tasks = [self.fetch_project(session, i) for i in range(self.__last_project_id)]
             results = await asyncio.gather(*tasks)
@@ -83,7 +63,7 @@ class Parser:
             with open("project_links.txt", mode="w", encoding="utf-8") as output_file:
                 for project_id, project_link in self.__projects_WEF:
                     output_file.write(str(project_id) + " | " + project_link + "\n")
-            return "'project_links.txt' was created"
+            return "File 'project_links.txt' was created"
         except Exception as e:
             return f"Exception caught: {e}"
 
